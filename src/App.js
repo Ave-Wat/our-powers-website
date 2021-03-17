aa289e396e3608f46b916f202c76fe4506cf391f
Ave-Wat <wattsa2@carleton.edu>
2021-03-17 10:06:56 -0500

    website outline

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
 import './App.css';
+import { NavLink, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='app'>
+      <header>
+        <h1>Our Powers</h1>
+        <Navigation/>
       </header>
+      <div className="pageContainer">
+        <Main/>
+        <Sidebar/>
+      </div>
+      <Footer/>
     </div>
   );
 }
+
+const Navigation = () => (
+  <nav>
+    <ul>
+      <li><NavLink to='/'>About</NavLink></li>
+      <li><NavLink to='/application'>Application</NavLink></li>
+      <li><NavLink to='/organization'>Organization</NavLink></li>
+      <li><NavLink to='/events'>Events</NavLink></li>
+      <li><NavLink to='/eligibility'>Eligibility</NavLink></li>
+    </ul>
+  </nav>
+);
+
+const About = () => (
+  <div className="pageContent">
+    about
+  </div>
+);
+
+const Application = () => (
+  <div className="pageContent">
+    application
+  </div>
+);
+
+const Organization = () => (
+  <div className="pageContent">
+    organization
+  </div>
+);
+
+const Events = () => (
+  <div className="pageContent">
+    events
+  </div>
+);
+
+const Eligibility = () => (
+  <div className="pageContent">
+    eligibility
+  </div>
+);
+
+const Sidebar = () => (
+  <div className="sidebar">
+    sidebar
+  </div>
+);
+
+const Footer = () => (
+  <div className="footer">
+    footer
+  </div>
+);
+
+const Main = () => (
+    <Switch>
+        <Route exact path='/' component={About}></Route>
+        <Route exact path='/application' component={Application}></Route>
+        <Route exact path='/organization' component={Organization}></Route>
+        <Route exact path='/events' component={Events}></Route>
+        <Route exact path='/eligibility' component={Eligibility}></Route>
+    </Switch>
+);
 
 export default App;
