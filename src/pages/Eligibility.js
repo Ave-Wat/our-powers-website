a36ba08627749844dbb3551f7ecb85859ab09a96
Ave-Wat <wattsa2@carleton.edu>
2021-05-07 16:56:06 -0500

    added downloadable consent from

--- src/pages/Eligibility.js
+++ src/pages/Eligibility.js
@@ -1,12 +1,13 @@
 import TextContainer from '../TextContainer';
 import Sidebar from '../Sidebar';
+import form from '../documents/consent_form.pdf'
 
 const Eligibility = () => (
   <div className="sidebarPageContainer">
     <div className="pageContent">
       <TextContainer
         title="Eligibility"
-        text="Any Ethiopian and/or Eritrean that identifies as a person passionate and interested in interacting with and expanding proper education regarding sexual and reproductive health representation is welcome to participate in the workshop. The topics covered in the workshop will lean heavily towards young women and children’s experiences in Ethiopian and Eritrean communities. This workshop will especially be useful for individuals with Ethiopian and Eritrean backgrounds. Please fill out this consent form before coming to the first meeting. If you need a hard copy sent to you, please let us know."
+        text={[<p>Any Ethiopian and/or Eritrean that identifies as a person passionate and interested in interacting with and expanding proper education regarding sexual and reproductive health representation is welcome to participate in the workshop. The topics covered in the workshop will lean heavily towards young women and children’s experiences in Ethiopian and Eritrean communities. This workshop will especially be useful for individuals with Ethiopian and Eritrean backgrounds. Please fill out this <a href={form}>consent form</a> before coming to the first meeting. If you need a hard copy sent to you, please let us know.</p>]}
       />
     </div>
     <Sidebar
