d4fade7c9322a93212f797ce0d798619fe725e05
Ave-Wat <wattsa2@carleton.edu>
2021-05-07 17:02:56 -0500

    redid link to pdf form

--- src/pages/Eligibility.js
+++ src/pages/Eligibility.js
@@ -7,7 +7,7 @@ const Eligibility = () => (
     <div className="pageContent">
       <TextContainer
         title="Eligibility"
-        text={[<p>Any Ethiopian and/or Eritrean that identifies as a person passionate and interested in interacting with and expanding proper education regarding sexual and reproductive health representation is welcome to participate in the workshop. The topics covered in the workshop will lean heavily towards young women and children’s experiences in Ethiopian and Eritrean communities. This workshop will especially be useful for individuals with Ethiopian and Eritrean backgrounds. Please fill out this <a href={form}>consent form</a> before coming to the first meeting. If you need a hard copy sent to you, please let us know.</p>]}
+        text={[<p>Any Ethiopian and/or Eritrean that identifies as a person passionate and interested in interacting with and expanding proper education regarding sexual and reproductive health representation is welcome to participate in the workshop. The topics covered in the workshop will lean heavily towards young women and children’s experiences in Ethiopian and Eritrean communities. This workshop will especially be useful for individuals with Ethiopian and Eritrean backgrounds. Please fill out this <a href={form} target="_blank">consent form</a> before coming to the first meeting. If you need a hard copy sent to you, please let us know.</p>]}
       />
     </div>
     <Sidebar
