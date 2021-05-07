faf86259dac7718f37bc98530f069141500af980
Ave-Wat <wattsa2@carleton.edu>
2021-05-07 10:30:56 -0500

    updated events

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -15,27 +15,36 @@ function Events () {
           <li>
           Week 1
           <p>Dates: June 30, 2021 & July 3, 2021</p>
-          <p>Topic: Reproductive Health and Contraceptive Methods</p> <p>Speakers:</p>
+          <p>Topic: Reproductive Health and Contraceptive Methods</p>
+          <p>Guest Speakers: Faris Cuchi</p>
+          <div><p>Faris is an intersectional LGBTIQ+/human rights activist based in Vienna, who oversees House of Guramayle’s activities in Austria. They are the co-host and creator of the Alen Show, which dismantles dishonest information about the Ethiopian LGBTIQ+ community one episode at a time. Faris is a computer science graduate and has worked as an IT expert with the INGO PSI-Ethiopia. They are also a Salzburg Global Seminar Fellow.*
+
+          Taken from the following website: https://houseofguramayle.org/our-story/.</p></div>
           </li>
           <li>
           Week 2
           <p>Dates: July 07, 2021 & July 10, 2021 </p>
-          <p>Topic:</p>
+          <p>Topic: Health Statistics in Ethiopia and Eritrea</p>
           </li>
           <li>
           Week 3
           <p>Dates: July 14, 2021 & July 17, 2021</p>
           <p>Topic: Rape Culture</p>
+          <p>Guest Speaker: Iman Mustafa</p>
+          <div><p>Iman is a Seattle-area organizer and activist dedicated to building the power of communities of color, incarcerated individuals, and immigrant communities. As a first-generation Black Muslim woman, Iman’s life experiences have given her insight into the systemic oppression that permeates the lives of the people she serves. In recognition of her work, Iman was selected as a 2019 Public Policy and International Affairs Fellow at the University of California, Berkeley studying the intersection of education and law. This propelled Iman into public service where she currently serves as part of the Rainier Beach Action Coalition Board, Puget Sound Community Leadership Institute Fellowship, and the George Jackson Freedom Coalition. She participates in a Black Dialogue Series Collective to facilitate dialogues on unity and collective action of the Black diaspora in her city and beyond. Most recently, she is a Podcast Founder & Co-Host of Keeping it Real with Mani and Lani which discusses our nation's most pressing policy and legal issues while celebrating the power of Black culture, media, and strategy. Iman is also a proud member of Zeta Phi Beta Sorority Incorporated.</p></div>
           </li>
           <li>
           Week 4
           <p>Dates: July 21, 2021 & July 24, 2021</p>
           <p>Topic: Gender Identity and Sexual Health</p>
+          <p>Guest Speakers: Faris Cuchi</p>
           </li>
           <li>
           Week 5
           <p>Dates: July 28, 2021 & July 31, 2021</p>
           <p>Topic: Mental Health and Resources</p>
+          <p>Guest Speaker: Lona Medhane</p>
+          <div><p>Lona is a Seattle-area activist dedicated to empowering marginalized communities through mental health education while promoting healthy trauma healing habits. As a first generation Black woman, Lona’s life experiences have led her to have a deeper understanding of how intergenerational trauma and mental illnesses can have negative long term effects on communities that don't have access to services and/or education to guide them through the hardships in familial and other relationships. In recognition of her work, she currently is a Podcast Founder & Co-Host of Keeping it Real with Mani and Lani  which opens discussion about the intersectionality of identities and oppression globally while also celebrating the power of Black culture, media, and strategy. Lona is also a proud member of Zeta Phi Beta Sorority Incorporated.</p></div>
           </li>
           </ul>]}
       />
